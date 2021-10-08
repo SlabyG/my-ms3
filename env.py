b9440d418705584ab99322ddab23065cba2dadff
SlabyG <gabriel17jh@gmail.com>
2021-10-08 10:30:12 +0000

    took out the env.py from the gitignore list in order to be able to submit the project

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "hE_blZ?5?ZH6US!e@B.VM3-ounha.n")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:rOOtUser@myfirstcluster.ztlbl.mongodb.net/myThirdDB?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "myThirdDB")
